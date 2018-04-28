4ebd4c07016c575a07dbb1671f76c6e1f3da4592
indraarianggi <indraarianggi@gmail.com>
2018-04-28 19:44:42 +0700

    spring starter 2

---
+++ program/ternaksapi_spring/src/ternaksapi_spring/config/SapiTableModel.java
@@ -0,0 +1,15 @@
+package ternaksapi_spring.config;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author indraarianggi
+ */
+public class SapiTableModel {
+    
+}
